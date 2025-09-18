5c2cb258956985dc18555816f2cd7d13045f6c0c
cfagerstrom0411 <123033405+cfagerstrom0411@users.noreply.github.com>
2025-09-17 20:57:04 -0400

    Add files via upload

---
+++ celcWebsite/main.js
@@ -0,0 +1,58 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "fa-solid fa-x" : "fa-solid fa-bars")
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars")
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+}
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header_content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header_content .header_btn", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about_card", {
+    duration: 1000,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".trainer_card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+const swiper = new Swiper(".swiper" , {
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+})
